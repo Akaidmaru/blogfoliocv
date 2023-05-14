002ada8655aa37d4bbf43cd2272d11ea35831139
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2023-05-13 21:09:45 -0500

    feat:  new classes for portfolio and cv models.
    Creation of new classes for both, portfolio and cv models. Just to have more information, it is still pending to check if this stays as an update or not.

--- cv/models.py
+++ cv/models.py
@@ -2,15 +2,41 @@ from django.db import models
 
 
 class Education(models.Model):
-    # degree
-    # major
-    # school
-    # start_date
+    institution_name = models.CharField(max_length=100)
+    degree = models.CharField(max_length=100)
+    field_of_study = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.institution_name
+
 
 class Experience(models.Model):
-    # title
-    # company
-    # location
-    # start_date
-    # end_date
-    # description
+    job_title = models.CharField(max_length=100)
+    employer = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description =  models.TextField(blank=True)
+
+    def __str__(self):
+        return self.job_title
+    
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Testimonial(models.Model):
+    author_name = models.CharField(max_length=100)
+    author_company = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.author_name
+    
--- portfolio/models.py
+++ portfolio/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='images/')
     url = models.URLField(blank=True)
+    technologies = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.title}"
